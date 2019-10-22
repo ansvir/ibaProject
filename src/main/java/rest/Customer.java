6b8b3e52ec57a36ebcef6feea7a95ed4368fb6d3
anton <svirepa.a@mail.ru>
2019-10-22 23:55:45 +0300

    initial commit

---
+++ src/main/java/rest/Customer.java
@@ -0,0 +1,41 @@
+package rest;
+
+public class Customer {
+
+    private Long id;
+    private String name;
+    private String url;
+
+    public Customer(long id, String name, String url) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+    }
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name=name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setId(String url) {
+        this.url=url;
+    }
+}
