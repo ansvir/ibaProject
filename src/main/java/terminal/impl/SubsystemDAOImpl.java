7845b092baaf5ca64cdf6c64efd5a44659b939a5
anton <svirepa.a@mail.ru>
2019-11-21 22:32:08 +0300

    commit 24

--- src/main/java/terminal/impl/SubsystemDAOImpl.java
+++ src/main/java/terminal/impl/SubsystemDAOImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import terminal.model.Command;
 import terminal.model.Subsystem;
 import terminal.dao.SubsystemDAO;
 import terminal.util.SubsystemRowMapper;
@@ -20,20 +19,8 @@ public class SubsystemDAOImpl implements SubsystemDAO{
         String query="SELECT * FROM subsystems WHERE id = ?";
         return jdbcTemplate.queryForObject(query,new SubsystemRowMapper(),id);
     }
-    public List<Subsystem> getAllSubsystems() {
-        Subsystem s=new Subsystem(1,"subName");
-        Subsystem s2=new Subsystem(2,"subName2");
-        Subsystem s4=new Subsystem(1,"subName");
-        Command c=new Command(2,1,"command","result","timestamp");
-        Command c2=new Command(4,1,"command2","result2","timestamp2");
-        Command c4=new Command(2,1,"command","result","timestamp");
-        System.out.println(s.toString());
-        System.out.println(c.toString());
-        System.out.println(s.equals(s2)+" "+s.equals(s4));
-        System.out.println(c.equals(c2)+" "+c.equals(c4));
-        System.out.println(s.hashCode());
-        System.out.println(c.hashCode());
 
+    public List<Subsystem> getAllSubsystems() {
         String query="SELECT * FROM subsystems;";
         return jdbcTemplate.query(query,new SubsystemRowMapper());
     }
